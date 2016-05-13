cfe54224df82710c854dd04b32eeed5876aa62f3
kamgroupboris <kamgroupboris@yandex.ru>
2016-05-13 14:11:23 +0300

    Правка Html меню согласно NavBar

---
+++ views/layouts/header.php
@@ -0,0 +1,40 @@
+	<div class="row headOne">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-1">
+					<button class="logoHome"><img src="img/Home.png" alt=""></button>
+				</div>
+				<div class="col-xs-3 headPhone">
+					<img src="img/phone1.png" alt="">
+					<a href="tel:8 (495) 150-22-93">8 (495) 150-22-93</a>
+				</div>
+				<div class="col-xs-2 backkol">
+					<button>Обратный звонок</button>
+				</div>
+				<div class="col-xs-4 headPhone">
+				<img src="img/mapPeaker.png" alt="">
+				<a href="https://www.google.ru/maps/place/%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B5+%D1%88.,+157,+%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0,+127566/@55.8992117,37.5386999,17z/data=!3m1!4b1!4m2!3m1!1s0x46b539e7fd86e527:0xa1a44ec91f228413" target="_blank">г.Москва, Дмитровское шоссе, 157</a>
+				</div>
+				<div class="col-xs-2 backkol prn">
+					<img src="img/clocker.png" alt="">
+					<p>Пн.-Пт. 9:00 - 20:00</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row headTwo">
+		<div class="container">
+			<div class="row">
+			<!--	<div class="col-xs-3 logoHeadTwo">
+					<a href="/"><img class="logo" src="img/asdad.svg" alt=""></a>
+				</div>
+				<div class="col-xs-7 navbar">-->
+					<?=$this->render('menuTop');?>	
+			<!--	</div>-->
+				<div class="col-xs-2 backkol2 prn">
+					<button><img src="img/analiz.png" alt="">Анализ сайта</button>
+					<button><img src="img/sendForMe.png" alt="">Напишите нам</button>
+				</div>
+			</div>
+		</div>
+	</div>
